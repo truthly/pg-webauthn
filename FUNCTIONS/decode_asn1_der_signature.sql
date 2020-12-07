578f1eda782485d98b38d04d99a1bed54ad9dede
Joel Jakobsson <joel@compiler.org>
2020-12-07 21:25:17 +0100

    No need for VALUES() here, can JOIN LATERAL directly on functions
    Author: Andreas Karlsson

--- FUNCTIONS/decode_asn1_der_signature.sql
+++ FUNCTIONS/decode_asn1_der_signature.sql
@@ -4,12 +4,12 @@ IMMUTABLE
 LANGUAGE sql
 AS $$
 SELECT integer1||integer2
-FROM (VALUES(get_byte(asn1der,3))) AS Q1(len1)
-JOIN LATERAL (VALUES(get_byte(asn1der,5+len1))) AS Q2(len2) ON TRUE
-JOIN LATERAL (VALUES(substring(asn1der from 5 for len1))) AS Q3(integer1_zeropadded) ON TRUE
-JOIN LATERAL (VALUES(substring(integer1_zeropadded from length(integer1_zeropadded)-31 for 32))) AS Q4(integer1) ON TRUE
-JOIN LATERAL (VALUES(substring(asn1der from 5+len1+2 for len2))) AS Q5(integer2_zeropadded) ON TRUE
-JOIN LATERAL (VALUES(substring(integer2_zeropadded from length(integer2_zeropadded)-31 for 32))) AS Q6(integer2) ON TRUE
+FROM get_byte(asn1der,3) AS Q1(len1)
+JOIN LATERAL get_byte(asn1der,5+len1) AS Q2(len2) ON TRUE
+JOIN LATERAL substring(asn1der from 5 for len1) AS Q3(integer1_zeropadded) ON TRUE
+JOIN LATERAL substring(integer1_zeropadded from length(integer1_zeropadded)-31 for 32) AS Q4(integer1) ON TRUE
+JOIN LATERAL substring(asn1der from 5+len1+2 for len2) AS Q5(integer2_zeropadded) ON TRUE
+JOIN LATERAL substring(integer2_zeropadded from length(integer2_zeropadded)-31 for 32) AS Q6(integer2) ON TRUE
 WHERE get_byte(asn1der,0) = 48 /* 0x30 SEQUENCE */
 AND get_byte(asn1der,1) = length(asn1der)-2
 AND get_byte(asn1der,2) = 2 /* 0x02 INTEGER */
