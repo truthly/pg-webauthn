93334cdc637b302449570f507a7cabe39e41684b
Joel Jakobsson <joel@compiler.org>
2020-12-07 21:24:17 +0100

    Use translate() instead of replace(replace())
    Author: Andreas Karlsson

--- FUNCTIONS/base64_url_decode.sql
+++ FUNCTIONS/base64_url_decode.sql
@@ -2,5 +2,5 @@ CREATE OR REPLACE FUNCTION webauthn.base64_url_decode(text)
 RETURNS bytea
 IMMUTABLE
 LANGUAGE sql AS $$
-SELECT decode(rpad(replace(replace($1,'-','+'),'_','/'),length($1) + (4 - length($1) % 4) % 4, '='),'base64')
+SELECT decode(rpad(translate($1,'-_','+/'),length($1) + (4 - length($1) % 4) % 4, '='),'base64')
 $$;
