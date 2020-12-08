88f00681c213356482e5a4bba1d84eaa81b53d59
Joel Jakobsson <joel@compiler.org>
2020-12-08 14:09:52 +0100

    Test as much output as possible from init_credential() and get_credentials()
    Before we ignored the output since it contained the random challenge value,
    which cannot be tested since it's random.
    
    Now, instead, replace the challenge in the output with null,
    allowing the rest of the json to be tested.
    
    Also, use jsonb_pretty() to canonicalize the json so it can be compared.

--- test.sql
+++ test.sql
@@ -11,7 +11,7 @@ SELECT set_config('request.header.X-Forwarded-For','127.0.0.1',FALSE);
 -- Replace random challenge with null to make test output predictable
 SELECT jsonb_pretty(jsonb_set(init_credential(username := 'test'), ARRAY['publicKey','challenge'], 'null'));
 
--- Override challenge with real challenge from observed logged traffic seen in Chrome -> Developer Tools -> Network
+-- Override challenge with real challenge from observed logged traffic seen in Chrome -> Developer Tools -> Network:
 UPDATE challenges SET
   challenge = decode('543amzXbkpA/g0DnFcvFvAybXBzwMg57fGed37YB8kM=','base64')
 WHERE challenge_id = (SELECT MAX(challenge_id) FROM challenges)
@@ -29,7 +29,7 @@ SELECT make_credential(
 -- Replace random challenge with null to make test output predictable
 SELECT jsonb_pretty(jsonb_set(get_credentials(username := 'test'), ARRAY['publicKey','challenge'], 'null'));
 
--- Override challenge with real challenge from observed logged traffic seen in Chrome -> Developer Tools -> Network
+-- Override challenge with real challenge from observed logged traffic seen in Chrome -> Developer Tools -> Network:
 UPDATE challenges SET
   challenge = webauthn.base64_url_decode(webauthn.from_utf8(decode('eyJ0eXBlIjoid2ViYXV0aG4uZ2V0IiwiY2hhbGxlbmdlIjoiVER5RnF4N2lwUS1Vb0otT3ZnWlVtV2ticHpPRlJtNk44WFY2bW93Sk9nUSIsIm9yaWdpbiI6Imh0dHA6Ly9sb2NhbGhvc3QiLCJjcm9zc09yaWdpbiI6ZmFsc2UsIm90aGVyX2tleXNfY2FuX2JlX2FkZGVkX2hlcmUiOiJkbyBub3QgY29tcGFyZSBjbGllbnREYXRhSlNPTiBhZ2FpbnN0IGEgdGVtcGxhdGUuIFNlZSBodHRwczovL2dvby5nbC95YWJQZXgifQ==','base64'))::jsonb->>'challenge')
 WHERE challenge_id = (SELECT MAX(challenge_id) FROM challenges)
@@ -42,6 +42,6 @@ SELECT verify_assertion(
   client_data_json := 'eyJ0eXBlIjoid2ViYXV0aG4uZ2V0IiwiY2hhbGxlbmdlIjoiVER5RnF4N2lwUS1Vb0otT3ZnWlVtV2ticHpPRlJtNk44WFY2bW93Sk9nUSIsIm9yaWdpbiI6Imh0dHA6Ly9sb2NhbGhvc3QiLCJjcm9zc09yaWdpbiI6ZmFsc2UsIm90aGVyX2tleXNfY2FuX2JlX2FkZGVkX2hlcmUiOiJkbyBub3QgY29tcGFyZSBjbGllbnREYXRhSlNPTiBhZ2FpbnN0IGEgdGVtcGxhdGUuIFNlZSBodHRwczovL2dvby5nbC95YWJQZXgifQ==',
   signature := 'MEUCIQD/y94itkRZrRcu5fQMplWcDorpCmpJ9YpnQvVgR/r5yAIgSy0nBbyWxFjH60R0u7ca27z4Ds/PiiycaYOeQxoB0nw=',
   user_handle := 'dGVzdA=='
-);
+) IS NOT NULL;
 
 ROLLBACK;
