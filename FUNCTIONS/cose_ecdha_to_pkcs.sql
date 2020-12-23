33f7142f6e36c103d9292c3d7ea02843b0aea220
Joel Jakobsson <joel@compiler.org>
2020-12-23 21:35:58 +0100

    Use the first decoded CBOR item since multiple might occur
    A CBOR object with two items occurred when testing a Yubikey
    in the requireResidentKey=TRUE mode.

--- FUNCTIONS/cose_ecdha_to_pkcs.sql
+++ FUNCTIONS/cose_ecdha_to_pkcs.sql
@@ -5,6 +5,6 @@ LANGUAGE sql
 AS $$
 -- https://github.com/fido-alliance/webauthn-demo/blob/master/utils.js#L105
 -- \x04 tag byte not prepended since not wanted by pg-ecdsa
-SELECT decode(cose_struct->>'-2','base64') || decode(cose_struct->>'-3','base64')
-FROM cbor.to_jsonb(cbor := cose_public_key, encode_binary_format := 'base64') AS cose_struct
+SELECT decode(cose_struct->0->>'-2','base64') || decode(cose_struct->0->>'-3','base64')
+FROM cbor.to_jsonb_array(cbor := cose_public_key, encode_binary_format := 'base64') AS cose_struct
 $$;
